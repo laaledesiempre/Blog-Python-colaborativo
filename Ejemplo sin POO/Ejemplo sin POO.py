98f431314e0726ab55c2cee8bf63bc85fa409619
Zzebastian <sebastian.scheverin@gmail.com>
2023-11-28 00:04:50 -0300

    Agregado de comentarios en el ejemplo

--- Ejemplo sin POO/Ejemplo sin POO.py
+++ Ejemplo sin POO/Ejemplo sin POO.py
@@ -1,8 +1,5 @@
 import sqlite3
 
-
-
-
 # Primero hay que crear la conección a la Base de datos
 conexion = sqlite3.connect("Blog-Python-colaborativo/Ejemplo sin POO/sinPOO.sqlite")
 
@@ -10,6 +7,7 @@ conexion = sqlite3.connect("Blog-Python-colaborativo/Ejemplo sin POO/sinPOO.sqli
 cursor = conexion.cursor()
 # Puede llamarse como se quiera, hasta nombre propio jaja
 
+# Código para crear la tabla "posteos" si no existe
 text = """CREATE TABLE IF NOT EXISTS posteos (
         idPost INTEGER PRIMARY KEY AUTOINCREMENT,
         titulo,
@@ -27,7 +25,7 @@ cursor.execute(text)
 # Guarda la ejecución de la mimsa (para que no se pierda)
 conexion.commit()
 
-
+# Función para crear un artículo
 def crearArticulo(titulo,contenido,autor,fecha,imagen,tags):
     articulo={}
     articulo["titulo"]=titulo
@@ -37,35 +35,29 @@ def crearArticulo(titulo,contenido,autor,fecha,imagen,tags):
     articulo["imagen"]=imagen
     articulo["tags"]=tags
     return articulo
-    
 
+# Función para insertar un artículo en la base de datos
 def insertar(articulo):
     global cursor
     text = """INSERT INTO posteos (titulo,contenido,autor,fecha,imagen,tags) VALUES  (?,?,?,?,?,?);"""
     data = (articulo["titulo"], articulo["contenido"],articulo["autor"],articulo["fecha"],articulo["imagen"],articulo["tags"])
-
     cursor.execute(text, data)
 
-
-    
+# Función para leer y mostrar todos los artículos
 def leer():
     global cursor
     cursor.execute("""SELECT * FROM posteos""")
     elementos = cursor.fetchall()
     for post in elementos:
         print(post)
-#
 
-
-
-
+# Crear un artículo de ejemplo y guardarlo en la base de datos
 art = crearArticulo("titulo","contenido","autor","fecha","imagen","tags")
-
-
 insertar(art)
 
-
+# Leer y mostrar todos los artículos de la base de datos
 leer()
 
+# Guardar los cambios en la base de datos y cerrar la conexión
 conexion.commit()
 conexion.close()
